2314db4a06040fdcbe886021adbbc96204c866e0
ahmed-magdy-gitt <ahmedmagdy707007@gmail.com>
2025-09-29 00:56:42 +0300

    edit file

--- sec3.java
+++ sec3.java
@@ -1,5 +1,6 @@
 public class sec3 {
     public static void main(String[] args) {
-        System.out.println("Hello, this is a test!");
+        System.out.println("Hello, this is a faking test!");
+        System.out.println("Hello, this is a faking test!");
     }
 }
